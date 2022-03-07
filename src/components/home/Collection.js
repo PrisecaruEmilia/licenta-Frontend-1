c7e5d708493871638760510e52031a4d21de0cdf
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-03-07 22:27:02 +0200

    21 - collection product component

--- src/components/home/Collection.js
+++ src/components/home/Collection.js
@@ -1,8 +1,140 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Col, Container, Row, Card } from 'react-bootstrap';
 
 export class Collection extends Component {
   render() {
-    return <div>Collection</div>;
+    return (
+      <Fragment>
+        <Container className="text-center" fluid={true}>
+          <div className="section-title text-center mb-55">
+            <h2>PRODUCT COLLECTION</h2>
+            <p>Some Of Our Exclusive Collection, You May Like</p>
+          </div>
+          <Row>
+            <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="image-box card w-100">
+                <img
+                  className="center w-75"
+                  src="https://www.epantofi.ro/media/catalog/product/cache/image/650x650/0/0/0000209901535_01_mk_1.jpg"
+                  alt="SNEAKERSI"
+                ></img>
+                <Card.Body>
+                  <p className="product-name-on-card">
+                    SNEAKERSI CASUAL NIKE COURT VISION LOW
+                  </p>
+                  <p className="product-price-on-card">Price : 320,00 lei</p>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="image-box card w-100">
+                <img
+                  className="center w-75"
+                  src="https://www.epantofi.ro/media/catalog/product/cache/image/650x650/0/0/0000209901535_01_mk_1.jpg"
+                  alt="SNEAKERSI"
+                ></img>
+                <Card.Body>
+                  <p className="product-name-on-card">
+                    SNEAKERSI CASUAL NIKE COURT VISION LOW
+                  </p>
+                  <p className="product-price-on-card">Price : 320,00 lei</p>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="image-box card w-100">
+                <img
+                  className="center w-75"
+                  src="https://www.epantofi.ro/media/catalog/product/cache/image/650x650/0/0/0000209901535_01_mk_1.jpg"
+                  alt="SNEAKERSI"
+                ></img>
+                <Card.Body>
+                  <p className="product-name-on-card">
+                    SNEAKERSI CASUAL NIKE COURT VISION LOW
+                  </p>
+                  <p className="product-price-on-card">Price : 320,00 lei</p>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="image-box card w-100">
+                <img
+                  className="center w-75"
+                  src="https://www.epantofi.ro/media/catalog/product/cache/image/650x650/0/0/0000209901535_01_mk_1.jpg"
+                  alt="SNEAKERSI"
+                ></img>
+                <Card.Body>
+                  <p className="product-name-on-card">
+                    SNEAKERSI CASUAL NIKE COURT VISION LOW
+                  </p>
+                  <p className="product-price-on-card">Price : 320,00 lei</p>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="image-box card w-100">
+                <img
+                  className="center w-75"
+                  src="https://www.epantofi.ro/media/catalog/product/cache/image/650x650/0/0/0000209901535_01_mk_1.jpg"
+                  alt="SNEAKERSI"
+                ></img>
+                <Card.Body>
+                  <p className="product-name-on-card">
+                    SNEAKERSI CASUAL NIKE COURT VISION LOW
+                  </p>
+                  <p className="product-price-on-card">Price : 320,00 lei</p>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="image-box card w-100">
+                <img
+                  className="center w-75"
+                  src="https://www.epantofi.ro/media/catalog/product/cache/image/650x650/0/0/0000209901535_01_mk_1.jpg"
+                  alt="SNEAKERSI"
+                ></img>
+                <Card.Body>
+                  <p className="product-name-on-card">
+                    SNEAKERSI CASUAL NIKE COURT VISION LOW
+                  </p>
+                  <p className="product-price-on-card">Price : 320,00 lei</p>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="image-box card w-100">
+                <img
+                  className="center w-75"
+                  src="https://www.epantofi.ro/media/catalog/product/cache/image/650x650/0/0/0000209901535_01_mk_1.jpg"
+                  alt="SNEAKERSI"
+                ></img>
+                <Card.Body>
+                  <p className="product-name-on-card">
+                    SNEAKERSI CASUAL NIKE COURT VISION LOW
+                  </p>
+                  <p className="product-price-on-card">Price : 320,00 lei</p>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="image-box card w-100">
+                <img
+                  className="center w-75"
+                  src="https://www.epantofi.ro/media/catalog/product/cache/image/650x650/0/0/0000209901535_01_mk_1.jpg"
+                  alt="SNEAKERSI"
+                ></img>
+                <Card.Body>
+                  <p className="product-name-on-card">
+                    SNEAKERSI CASUAL NIKE COURT VISION LOW
+                  </p>
+                  <p className="product-price-on-card">Price : 320,00 lei</p>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </Fragment>
+    );
   }
 }
 
