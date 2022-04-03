48669d4eac1e364a911bc965c0784ea3d108ffee
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-04-03 17:58:57 +0300

    23 - user login register component

---
+++ src/pages/UserLoginPage.js
@@ -0,0 +1,34 @@
+import React, { Component, Fragment } from 'react';
+import Footer from '../components/common/Footer';
+import FooterMobile from '../components/common/FooterMobile';
+import NavMenu from '../components/common/NavMenu';
+import NavMenuMobile from '../components/common/NavMenuMobile';
+import UserLogin from '../components/common/UserLogin';
+
+class UserLoginPage extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div className="Desktop">
+          <NavMenu />
+        </div>
+
+        <div className="Mobile">
+          <NavMenuMobile />
+        </div>
+
+        <UserLogin />
+
+        <div className="Desktop">
+          <Footer />
+        </div>
+
+        <div className="Mobile">
+          <FooterMobile />
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default UserLoginPage;
