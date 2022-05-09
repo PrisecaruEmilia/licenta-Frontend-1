c9f24530ab78a1e51b0b597956188ab0bebc78f9
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-05-09 17:35:14 +0300

    Consume Site Info API in Client Side Part 5

--- src/components/others/Privacy.js
+++ src/components/others/Privacy.js
@@ -1,7 +1,35 @@
 import React, { Component, Fragment } from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
-
+import AppURL from '../../api/AppUrl';
+import axios from 'axios';
+import ReactHtmlParser from 'react-html-parser';
 class Privacy extends Component {
+  constructor() {
+    super();
+    this.state = {
+      privacy: '',
+      loaderDiv: '',
+      mainDiv: 'd-none',
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(AppURL.AllSiteInfo)
+      .then((response) => {
+        let StatusCode = response.status;
+        if (StatusCode == 200) {
+          let JsonData = response.data[0]['privacy'];
+          this.setState({
+            privacy: JsonData,
+            loaderDiv: 'd-none',
+            mainDiv: '',
+          });
+        }
+      })
+      .catch((error) => {});
+  }
+
   render() {
     return (
       <Fragment>
@@ -14,24 +42,44 @@ class Privacy extends Component {
               sm={12}
               xs={12}
             >
-              <h4 className="section-title-login">Privacy Page </h4>
-              <p className="section-title-contact">
-                Hi! I'm Jane Doe. I'm a web developer with a serious love for
-                teaching I am a founder of eLe easy Learning and a passionate
-                Web Developer, Programmer and Instructor.<br></br>
-                <br></br>I am working online for the last 7 years and have
-                created several successful websites running on the internet. I
-                try to create a project-based course that helps you to learn
-                professionally and make you fell as a complete developer. easy
-                learning exists to help you succeed in life.<br></br>
-                <br></br>
-                Each course has been hand-tailored to teach a specific skill. I
-                hope you agree! Whether you’re trying to learn a new skill from
-                scratch or want to refresh your memory on something you’ve
-                learned in the past, you’ve come to the right place.<br></br>
-                Education makes the world a better place. Make your world better
-                with new skills.
-              </p>
+              <div className={this.state.loaderDiv}>
+                <div className="ph-item">
+                  <div className="ph-col-12">
+                    <div className="ph-row">
+                      <div className="ph-col-4"></div>
+                      <div className="ph-col-8 empty"></div>
+                      <div className="ph-col-6"></div>
+                      <div className="ph-col-6 empty"></div>
+                      <div className="ph-col-12"></div>
+                      <div className="ph-col-12"></div>
+                      <div className="ph-col-12"></div>
+                      <div className="ph-col-12"></div>
+                    </div>
+                  </div>
+                </div>
+                <div className="ph-item">
+                  <div className="ph-col-12">
+                    <div className="ph-row">
+                      <div className="ph-col-4"></div>
+                      <div className="ph-col-8 empty"></div>
+                      <div className="ph-col-6"></div>
+                      <div className="ph-col-6 empty"></div>
+                      <div className="ph-col-12"></div>
+                      <div className="ph-col-12"></div>
+                      <div className="ph-col-12"></div>
+                      <div className="ph-col-12"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className={this.state.mainDiv}>
+                <h4 className="section-title-login">Privacy Page </h4>
+                <div className="section-title-contact">
+                  {' '}
+                  {ReactHtmlParser(this.state.privacy)}{' '}
+                </div>
+              </div>
             </Col>
           </Row>
         </Container>
