0d30f2286ba54e26e1cb027be1e84bc71a43e05d
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-05-10 21:20:30 +0300

    30 - Consume Product List API Part 2

--- src/components/home/Collection.js
+++ src/components/home/Collection.js
@@ -1,137 +1,70 @@
 import React, { Component, Fragment } from 'react';
 import { Col, Container, Row, Card } from 'react-bootstrap';
-
+import AppURL from '../../api/AppUrl';
+import axios from 'axios';
 export class Collection extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ProductData: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(AppURL.ProductListByRemark('COLLECTION'))
+      .then((response) => {
+        this.setState({ ProductData: response.data });
+      })
+      .catch((error) => {});
+  }
   render() {
+    const CollectionList = this.state.ProductData;
+    const MyView = CollectionList.map((CollectionList, i) => {
+      if (CollectionList.special_price == 'na') {
+        return (
+          <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
+            <Card className="image-box card w-100">
+              <img className="center w-75" src={CollectionList.image} />
+              <Card.Body>
+                <p className="product-name-on-card">{CollectionList.title}</p>
+                <p className="product-price-on-card">
+                  Price : ${CollectionList.price}
+                </p>
+              </Card.Body>
+            </Card>
+          </Col>
+        );
+      } else {
+        return (
+          <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
+            <Card className="image-box card w-100">
+              <img className="center w-75" src={CollectionList.image} />
+              <Card.Body>
+                <p className="product-name-on-card">{CollectionList.title}</p>
+                <p className="product-price-on-card">
+                  Price :{' '}
+                  <strike className="text-secondary">
+                    ${CollectionList.price}
+                  </strike>{' '}
+                  ${CollectionList.special_price}
+                </p>
+              </Card.Body>
+            </Card>
+          </Col>
+        );
+      }
+    });
+
     return (
       <Fragment>
         <Container className="text-center" fluid={true}>
           <div className="section-title text-center mb-55">
-            <h2>PRODUCT COLLECTION</h2>
+            <h2> PRODUCT COLLECTION</h2>
             <p>Some Of Our Exclusive Collection, You May Like</p>
           </div>
-          <Row>
-            <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
-              <Card className="image-box card w-100">
-                <img
-                  className="center w-75"
-                  src="https://www.epantofi.ro/media/catalog/product/cache/image/650x650/0/0/0000209901535_01_mk_1.jpg"
-                  alt="SNEAKERSI"
-                ></img>
-                <Card.Body>
-                  <p className="product-name-on-card">
-                    SNEAKERSI CASUAL NIKE COURT VISION LOW
-                  </p>
-                  <p className="product-price-on-card">Price : 320,00 lei</p>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
-              <Card className="image-box card w-100">
-                <img
-                  className="center w-75"
-                  src="https://www.epantofi.ro/media/catalog/product/cache/image/650x650/0/0/0000209901535_01_mk_1.jpg"
-                  alt="SNEAKERSI"
-                ></img>
-                <Card.Body>
-                  <p className="product-name-on-card">
-                    SNEAKERSI CASUAL NIKE COURT VISION LOW
-                  </p>
-                  <p className="product-price-on-card">Price : 320,00 lei</p>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
-              <Card className="image-box card w-100">
-                <img
-                  className="center w-75"
-                  src="https://www.epantofi.ro/media/catalog/product/cache/image/650x650/0/0/0000209901535_01_mk_1.jpg"
-                  alt="SNEAKERSI"
-                ></img>
-                <Card.Body>
-                  <p className="product-name-on-card">
-                    SNEAKERSI CASUAL NIKE COURT VISION LOW
-                  </p>
-                  <p className="product-price-on-card">Price : 320,00 lei</p>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
-              <Card className="image-box card w-100">
-                <img
-                  className="center w-75"
-                  src="https://www.epantofi.ro/media/catalog/product/cache/image/650x650/0/0/0000209901535_01_mk_1.jpg"
-                  alt="SNEAKERSI"
-                ></img>
-                <Card.Body>
-                  <p className="product-name-on-card">
-                    SNEAKERSI CASUAL NIKE COURT VISION LOW
-                  </p>
-                  <p className="product-price-on-card">Price : 320,00 lei</p>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
-              <Card className="image-box card w-100">
-                <img
-                  className="center w-75"
-                  src="https://www.epantofi.ro/media/catalog/product/cache/image/650x650/0/0/0000209901535_01_mk_1.jpg"
-                  alt="SNEAKERSI"
-                ></img>
-                <Card.Body>
-                  <p className="product-name-on-card">
-                    SNEAKERSI CASUAL NIKE COURT VISION LOW
-                  </p>
-                  <p className="product-price-on-card">Price : 320,00 lei</p>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
-              <Card className="image-box card w-100">
-                <img
-                  className="center w-75"
-                  src="https://www.epantofi.ro/media/catalog/product/cache/image/650x650/0/0/0000209901535_01_mk_1.jpg"
-                  alt="SNEAKERSI"
-                ></img>
-                <Card.Body>
-                  <p className="product-name-on-card">
-                    SNEAKERSI CASUAL NIKE COURT VISION LOW
-                  </p>
-                  <p className="product-price-on-card">Price : 320,00 lei</p>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
-              <Card className="image-box card w-100">
-                <img
-                  className="center w-75"
-                  src="https://www.epantofi.ro/media/catalog/product/cache/image/650x650/0/0/0000209901535_01_mk_1.jpg"
-                  alt="SNEAKERSI"
-                ></img>
-                <Card.Body>
-                  <p className="product-name-on-card">
-                    SNEAKERSI CASUAL NIKE COURT VISION LOW
-                  </p>
-                  <p className="product-price-on-card">Price : 320,00 lei</p>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
-              <Card className="image-box card w-100">
-                <img
-                  className="center w-75"
-                  src="https://www.epantofi.ro/media/catalog/product/cache/image/650x650/0/0/0000209901535_01_mk_1.jpg"
-                  alt="SNEAKERSI"
-                ></img>
-                <Card.Body>
-                  <p className="product-name-on-card">
-                    SNEAKERSI CASUAL NIKE COURT VISION LOW
-                  </p>
-                  <p className="product-price-on-card">Price : 320,00 lei</p>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
+
+          <Row>{MyView}</Row>
         </Container>
       </Fragment>
     );
