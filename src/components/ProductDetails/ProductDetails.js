c9cc488a2294b3841e051029aab745f2190dac98
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-05-12 12:56:51 +0300

    33 - Consume Product Details API Part 3

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -6,6 +6,31 @@ import Product3 from '../../assets/images/product/product3.png';
 import Product4 from '../../assets/images/product/product4.png';
 class ProductDetails extends Component {
   render() {
+    let ProductAllData = this.props.data;
+    let title = ProductAllData['productList'][0]['title'];
+    let brand = ProductAllData['productList'][0]['brand'];
+    let category = ProductAllData['productList'][0]['category'];
+    let subcategory = ProductAllData['productList'][0]['subcategory'];
+    let image = ProductAllData['productList'][0]['image'];
+
+    let price = ProductAllData['productList'][0]['price'];
+    let product_code = ProductAllData['productList'][0]['product_code'];
+    let remark = ProductAllData['productList'][0]['remark'];
+    let special_price = ProductAllData['productList'][0]['special_price'];
+    let star = ProductAllData['productList'][0]['star'];
+
+    let image_one = ProductAllData['productDetails'][0]['image_one'];
+    let image_two = ProductAllData['productDetails'][0]['image_two'];
+    let image_three = ProductAllData['productDetails'][0]['image_three'];
+    let image_four = ProductAllData['productDetails'][0]['image_four'];
+    let color = ProductAllData['productDetails'][0]['color'];
+    let size = ProductAllData['productDetails'][0]['size'];
+
+    let product_id = ProductAllData['productDetails'][0]['product_id'];
+    let short_description =
+      ProductAllData['productDetails'][0]['short_description'];
+    let long_description =
+      ProductAllData['productDetails'][0]['long_description'];
     return (
       <Fragment>
         <Container fluid={true} className="BetweenTwoSection">
@@ -19,44 +44,42 @@ class ProductDetails extends Component {
             >
               <Row>
                 <Col className="p-3" md={6} lg={6} sm={12} xs={12}>
-                  <img className="w-100" src={Product1} />
+                  <img className="w-100" src={image_one} />
                   <Container className="my-3">
                     <Row>
                       <Col className="p-0 m-0" md={3} lg={3} sm={3} xs={3}>
-                        <img className="w-100" src={Product1} />
+                        <img className="w-100" src={image_one} />
                       </Col>
                       <Col className="p-0 m-0" md={3} lg={3} sm={3} xs={3}>
-                        <img className="w-100" src={Product2} />
+                        <img className="w-100" src={image_two} />
                       </Col>
                       <Col className="p-0 m-0" md={3} lg={3} sm={3} xs={3}>
-                        <img className="w-100" src={Product3} />
+                        <img className="w-100" src={image_three} />
                       </Col>
                       <Col className="p-0 m-0" md={3} lg={3} sm={3} xs={3}>
-                        <img className="w-100" src={Product4} />
+                        <img className="w-100" src={image_four} />
                       </Col>
                     </Row>
                   </Container>
                 </Col>
                 <Col className="p-3 " md={6} lg={6} sm={12} xs={12}>
-                  <h5 className="Product-Name">
-                    ASUS TUF A15 FA506IU Ryzen 7 4800H GTX
-                  </h5>
-                  <h6 className="section-sub-title">
-                    Some Of Our Exclusive Collection, You May Like Some Of Our
-                    Exclusive Collectio
-                  </h6>
+                  <h5 className="Product-Name">{title}</h5>
+                  <h6 className="section-sub-title">{short_description}</h6>
                   <div className="input-group">
                     <div className="Product-price-card d-inline ">
-                      Reguler Price 200
+                      Reguler Price ${price}
                     </div>
                     <div className="Product-price-card d-inline ">
                       50% Discount
                     </div>
                     <div className="Product-price-card d-inline ">
-                      New Price 100
+                      New Price ${special_price}
                     </div>
                   </div>
-                  <h6 className="mt-2">Choose Color</h6>
+                  <h6 className="mt-2">
+                    Category : <b>{category}</b>{' '}
+                  </h6>
+                  {/* <h6 className="mt-2">Choose Color</h6>
                   <div className="input-group">
                     <div className="form-check mx-1">
                       <input
@@ -103,9 +126,19 @@ class ProductDetails extends Component {
                         Red
                       </label>
                     </div>
-                  </div>
+                  </div> */}
+                  <h6 className="mt-2">
+                    SubCategory : <b>{subcategory}</b>
+                  </h6>
 
-                  <h6 className="mt-2">Choose Size</h6>
+                  <h6 className="mt-2">
+                    Brand : <b>{brand}</b>
+                  </h6>
+
+                  <h6 className="mt-2">
+                    Product Code : <b>{product_code}</b>
+                  </h6>
+                  {/* <h6 className="mt-2">Choose Size</h6>
                   <div className="input-group">
                     <div className="form-check mx-1">
                       <input
@@ -152,13 +185,13 @@ class ProductDetails extends Component {
                         XXXX
                       </label>
                     </div>
-                  </div>
+                  </div> */}
 
-                  <h6 className="mt-2">Quantity</h6>
+                  {/* <h6 className="mt-2">Quantity</h6>
                   <input
                     className="form-control text-center w-50"
                     type="number"
-                  />
+                  /> */}
 
                   <div className="input-group mt-3">
                     <button className="btn site-btn m-1 ">
@@ -180,24 +213,7 @@ class ProductDetails extends Component {
               <Row>
                 <Col className="" md={6} lg={6} sm={12} xs={12}>
                   <h6 className="mt-2">DETAILS</h6>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
-                    sed diam nonummy nibh euismod tincidunt ut laoreet dolore
-                    magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
-                    quis nostrud exerci tation Lorem ipsum dolor sit amet,
-                    consectetuer adipiscing elit, sed diam nonummy nibh euismod
-                    tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut
-                    wisi enim ad minim veniam, quis nostrud exerci tation
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
-                    sed diam nonummy nibh euismod tincidunt ut laoreet dolore
-                    magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
-                    quis nostrud exerci tation Lorem ipsum dolor sit amet,
-                    consectetuer adipiscing elit, sed diam nonummy nibh euismod
-                    tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut
-                    wisi enim ad minim veniam, quis nostrud exerci tation
-                  </p>
+                  <p> {long_description} </p>
                 </Col>
 
                 <Col className="" md={6} lg={6} sm={12} xs={12}>
